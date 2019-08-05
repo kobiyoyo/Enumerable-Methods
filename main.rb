2a5e5f1a2e6b381bbf5fba1e146286f4d41445c3
Chubi Adama <adamachubi@gmail.com>
2019-08-05 14:40:17 +0100

    added the remain methods

--- main.rb
+++ main.rb
@@ -13,8 +13,6 @@ module Enumerable
         end
     end
     
-    
-    
     def my_select
         new_array = []
         self.my_each{|x|
@@ -25,11 +23,58 @@ module Enumerable
         return new_array
     end
     
-    
     def my_all?
         self.my_each{|x|
             return false if !yield(x)
             return true
             }
     end
+    
+    def my_any?
+        new_array = []
+        self.my_each{|x|
+            if yield(x)
+                new_array << x 
+            end
+            }
+        return new_array
+    end
+    
+    def my_none?
+        new_array = []
+        self.my_each{|x|
+            if !yield(x)
+                new_array << x  
+            end   
+            }    
+        return new_array
+    end
+
+    def my_count
+        counter = 0
+        self.my_each do |x|
+            if !block_given?
+                return self.length
+            elsif yield(x) 
+                counter += 1
+            end
+        end
+        return counter
+    end
+
+    def my_map(&block)
+        new_arry = []
+        self.my_each{|x|
+            new_arry << block.call(x)
+            }
+        return new_arry
+    end
+
+    def my_inject(accum)
+        self.my_each{|x|
+            accum = yield(accum,x)
+            }
+        return accum
+    end
+ 
 end
