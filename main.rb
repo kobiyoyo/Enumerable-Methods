ee1fadf648ea6835e35d8d7eef4ea540891d36c6
Chubi Adama <adamachubi@gmail.com>
2019-08-05 14:06:20 +0100

    added my_select and my_all methods

--- main.rb
+++ main.rb
@@ -4,9 +4,32 @@ module Enumerable
             yield(self[i])
         end
     end
+    
+    
+    
     def my_each_with_index
         for i in 0...self.length
             yield(self[i],i)
         end
+    end
+    
+    
+    
+    def my_select
+        new_array = []
+        self.my_each{|x|
+            if yield(x)
+                new_array << x 
+            end        
+        }
+        return new_array
+    end
+    
+    
+    def my_all?
+        self.my_each{|x|
+            return false if !yield(x)
+            return true
+            }
     end
 end
